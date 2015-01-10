761b81e3343b1268365dc6fef6d9d2fc5769340f
s_anno <amo.20325@gmail.com>
2015-01-10 15:17:09 +0900

    Initial Commit

---
+++ Either/Either.h
@@ -0,0 +1,19 @@
+//
+//  Either.h
+//  Either
+//
+//  Created by amo on 2015/01/10.
+//  Copyright (c) 2015年 amo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Either.
+FOUNDATION_EXPORT double EitherVersionNumber;
+
+//! Project version string for Either.
+FOUNDATION_EXPORT const unsigned char EitherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Either/PublicHeader.h>
+
+
